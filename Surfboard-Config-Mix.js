436158d8677cf46935de1d2fea62dae9a936a6c8
wddclass <rocytm@vip.qq.com>
2024-01-20 21:41:53 +0800

    fix: surfboard ua

--- Surfboard-Config-Mix.js
+++ Surfboard-Config-Mix.js
@@ -13,82 +13,83 @@
  */
 
 export default {
-  async fetch(request) {
+	async fetch(request) {
 
-    const urlParams = new URL(request.url).searchParams
-    const param_template_url = urlParams.get('template')
-    const param_file_name = urlParams.get('name')
-    const param_url = urlParams.get('url')
+		const urlParams = new URL(request.url).searchParams
+		const param_template_url = urlParams.get('template')
+		const param_file_name = urlParams.get('name')
+		const param_url = urlParams.get('url')
 
-    if (!param_template_url) {
-      return new Response('请传入template参数')
-    }
-    if (!param_file_name) {
-      return new Response('请传入name参数')
-    }
-    if (!param_url) {
-      return new Response('请传入url参数')
-    }
+		if (!param_template_url) {
+			return new Response('请传入template参数')
+		}
+		if (!param_file_name) {
+			return new Response('请传入name参数')
+		}
+		if (!param_url) {
+			return new Response('请传入url参数')
+		}
 
-    const templateResponse = await fetch(param_template_url)
-    const template_context = await templateResponse.text()
+		const templateResponse = await fetch(param_template_url)
+		const template_context = await templateResponse.text()
 
-    const response = await fetch(param_url, {
-      headers: {
-        "User-Agent": "Surfboard"
-      }
-    })
-    const subscriptionUserinfo = response.headers.get("subscription-userinfo")
+		const response = await fetch(param_url, {
+			headers: {
+				"User-Agent": "clash"
+			}
+		})
+		const subscriptionUserinfo = response.headers.get("subscription-userinfo")
 
-    const modifiedHeaders = new Headers()
-    modifiedHeaders.set('Content-Type', 'text/plain')
-    modifiedHeaders.set('content-disposition', `attachment; filename=${param_file_name}`)
+		const modifiedHeaders = new Headers()
+		modifiedHeaders.set('Content-Type', 'text/plain')
+		modifiedHeaders.set('content-disposition', `attachment; filename=${param_file_name}`)
 
-    let surfboard_content = await template_context
-    surfboard_content = surfboard_content.replace(/policy-path = .*?,/g, `policy-path = ${param_url},`);
+		let surfboard_content = await template_context
+		surfboard_content = surfboard_content.replace(/policy-path = .*?,/g, `policy-path = ${param_url},`);
+		
+		let subList = []
+		if (subscriptionUserinfo) {
+			subList = subscriptionUserinfo.split(';').map((item) => {
+				var parts = item.split("=");
+				return { name: parts[0].trim(), value: parts[1] };
+			});
+			let subUpload = '',
+				subDownload = '',
+				subTotal = '',
+				subDate = '',
+				subUsed = ''
+			try {
+				subUpload = (Number(subList.find(item => item.name == 'upload').value) / (1024 * 1024 * 1024)).toFixed(2)
+				subDownload = (Number(subList.find(item => item.name == 'download').value) / (1024 * 1024 * 1024)).toFixed(2)
+				subTotal = (Number(subList.find(item => item.name == 'total').value) / (1024 * 1024 * 1024)).toFixed(2)
+				subDate = formatDate(Number(subList.find(item => item.name == 'expire').value) * 1000)
+				subUsed = (Number(subUpload) + Number(subDownload)).toFixed(2)
+				// let subDate = subList.find(item => item.name == 'expire').value
+			} catch { }
 
-    let subList = []
-    // if (subscriptionUserinfo)
-    subList = subscriptionUserinfo.split(';').map((item) => {
-      var parts = item.split("=");
-      return { name: parts[0].trim(), value: parts[1] };
-    });
-    let subUpload = '',
-      subDownload = '',
-      subTotal = '',
-      subDate = '',
-      subUsed = ''
-    try {
-      subUpload = (Number(subList.find(item => item.name == 'upload').value) / (1024 * 1024 * 1024)).toFixed(2)
-      subDownload = (Number(subList.find(item => item.name == 'download').value) / (1024 * 1024 * 1024)).toFixed(2)
-      subTotal = (Number(subList.find(item => item.name == 'total').value) / (1024 * 1024 * 1024)).toFixed(2)
-      subDate = formatDate(Number(subList.find(item => item.name == 'expire').value) * 1000)
-      subUsed = (Number(subUpload) + Number(subDownload)).toFixed(2)
-      // let subDate = subList.find(item => item.name == 'expire').value
-    } catch { }
 
+			let SubscribeInfo = `上传流量：${subUpload}GB\\n下载流量：${subDownload}GB\\n已用流量：${subUsed}GB\\n套餐流量：${subTotal}GB\\n到期时间：${subDate}`
+			let PanelInfo = `SubscribeInfo = title=${param_file_name}订阅信息, content=${SubscribeInfo}, style=info\n`
 
-    let SubscribeInfo = `上传流量：${subUpload}GB\\n下载流量：${subDownload}GB\\n已用流量：${subUsed}GB\\n套餐流量：${subTotal}GB\\n到期时间：${subDate}`
-    let PanelInfo = `SubscribeInfo = title=${param_file_name}订阅信息, content=${SubscribeInfo}, style=info\n`
+			surfboard_content = surfboard_content.replace(/SubscribeInfo.*/, PanelInfo);
+		}
+		surfboard_content = surfboard_content.replace(/^.*/, `#!MANAGED-CONFIG ${param_template_url} interval=43200 strict=true`)
 
-    surfboard_content = surfboard_content.replace(/SubscribeInfo.*/, PanelInfo);
-    surfboard_content = surfboard_content.replace(/^.*/, `#!MANAGED-CONFIG ${param_template_url} interval=43200 strict=true`)
+		return new Response(surfboard_content)
 
-    return new Response(surfboard_content)
+		function formatDate(timestamp) {
+			var date = new Date(timestamp);
 
-    function formatDate(timestamp) {
-      var date = new Date(timestamp);
+			var year = date.getFullYear();
+			var month = ("0" + (date.getMonth() + 1)).slice(-2);
+			var day = ("0" + date.getDate()).slice(-2);
+			var hours = ("0" + date.getHours()).slice(-2);
+			var minutes = ("0" + date.getMinutes()).slice(-2);
+			var seconds = ("0" + date.getSeconds()).slice(-2);
 
-      var year = date.getFullYear();
-      var month = ("0" + (date.getMonth() + 1)).slice(-2);
-      var day = ("0" + date.getDate()).slice(-2);
-      var hours = ("0" + date.getHours()).slice(-2);
-      var minutes = ("0" + date.getMinutes()).slice(-2);
-      var seconds = ("0" + date.getSeconds()).slice(-2);
+			var formattedDate = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
 
-      var formattedDate = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
-
-      return formattedDate
-    }
-  },
+			return formattedDate
+		}
+	},
 };
